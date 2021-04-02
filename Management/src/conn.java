b87a158d5b8ac2c89d9105f8f3f2edd8e2d55f8f
Isha307 <56998032+Isha307@users.noreply.github.com>
2021-04-02 20:35:46 +0530

    Update conn.java

--- Management/src/conn.java
+++ Management/src/conn.java
@@ -14,8 +14,8 @@ public Connection c;
 public Statement s;
 public conn(){
   try{ 
-   Class.forName("com.mysql.jdbc.Driver");
-   c= DriverManager.getConnection("jdbc:mysql:///employee","root","");
+   Class.forName("com.mysql.cj.jdbc.Driver");
+   c= DriverManager.getConnection("jdbc:mysql://employee","root","");
    s=c.createStatement();
 }
   catch(Exception e){
